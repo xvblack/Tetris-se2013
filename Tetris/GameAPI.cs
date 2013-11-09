3a1e3e6dc1a1ff9240af798bb2ed07480797b032
xvblack <xvblue@gmail.com>
2013-11-09 23:47:18 +0800

    Many Things
    改的有点多：
    1、加入了计分的API
    2、加入了游戏逻辑的主要类Tetrisor TODO
    3、更改了定位方式，现在的方式为左下定位，这样最上一行有隐藏的一行保证仅有一行还是可以生成方块

---
+++ Tetris/GameAPI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.GameBase
+{
+    public partial class TetrisGame
+    {
+        #region TickAPI
+        public int TickScore
+        {
+            get;
+            private set;
+        }
+
+
+        private void InitTickAPI()
+        {
+            TickScore = 0;
+            ClearBarEvent += TickOnClearBar;
+            UpdateBeginEvent += TickOnUpdateBegin;
+        }
+
+        private void TickOnClearBar(object sender, ClearBarEventArgs e)
+        {
+            TickScore++;
+        }
+
+        private void TickOnUpdateBegin(object sender, UpdateBeginEventArgs e)
+        {
+            TickScore = 0;
+        }
+        #endregion
+        #region UnderlyingAPI
+
+        public int UpperBound
+        {
+            get
+            {
+                return 0;
+            }
+        }
+        #endregion
+    }
+}
