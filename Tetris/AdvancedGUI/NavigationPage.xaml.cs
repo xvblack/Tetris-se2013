291e22b16c38252ba8f5330529ed50b5918f2d5d
shuohan <hsh_ufo@126.com>
2013-12-12 23:08:16 +0800

    Add a navigation page
    I added a navigation page. I have not finished yet. It is still ugly now
    and I have not turn the game window into a page.

---
+++ Tetris/AdvancedGUI/NavigationPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tetris.AdvancedGUI
+{
+    /// <summary>
+    /// NavigationPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class NavigationPage : Page
+    {
+        NavigationService nav;
+
+        public NavigationPage()
+        {
+            InitializeComponent();
+        }
+
+        // Click the button to go to Achievement
+        private void gotoAchievement_Click(object sender, RoutedEventArgs e)
+        {
+            nav = NavigationService.GetNavigationService(this);
+            nav.Navigate(new AchievementPage());
+        }
+        // Click the button to go to GameModeSelection Page
+        private void gotoGameModeSel_Click(object sender, RoutedEventArgs e)
+        {
+            nav = NavigationService.GetNavigationService(this);
+            nav.Navigate(new GameModeSelectionPage());
+        }
+        // Click the button to go to Setting Page
+        private void gotoSetting_Click(object sender, RoutedEventArgs e)
+        {
+            nav = NavigationService.GetNavigationService(this);
+            nav.Navigate(new SettingPage());
+        }
+        // Click the button to exit the game
+        private void exitGame_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
---
+++ Tetris/AdvancedGUI/SettingPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tetris.AdvancedGUI
+{
+    /// <summary>
+    /// SettingPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingPage : Page
+    {
+        NavigationService nav;
+        
+        public SettingPage()
+        {
+            InitializeComponent();
+        }
+
+        // NOT FINISEHD!!!
+
+        // Click to start single mode game
+        private void selSingleMode_Click(object sender, RoutedEventArgs e)
+        {
+            nav = NavigationService.GetNavigationService(this);
+            //nav.Navigate(new singleModePage());
+        }
+
+        // Click to start dual mode game
+        private void selDualMode_Click(object sender, RoutedEventArgs e)
+        {
+            nav = NavigationService.GetNavigationService(this);
+            //nav.Navigate(new dualModePage());
+        }
+        // Click to go to the last page
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            nav = NavigationService.GetNavigationService(this);
+            nav.Navigate(new NavigationPage());
+        }
+    }
+}
