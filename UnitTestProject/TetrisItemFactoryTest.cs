fdac18499100dc883fc1eb62928aa48235bc7fcc
xvblack <xvblue@gmail.com>
2013-12-25 19:12:59 +0800

    Add a unit test of block

---
+++ UnitTestProject/TetrisItemFactoryTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tetris;
+using Tetris.GameBase;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TetrisItemFactoryTest
+    {
+        private static TetrisGame _game;
+        private static ITetrisFactory _factory;
+        [ClassInitialize]
+        public static void SetupFactory()
+        {
+            _game = (new Tetrisor()).NewGame();
+            _factory = _game.Factory;
+        }
+        [TestMethod]
+        public void TestGenItemSquare()
+        {
+            Assert.IsInstanceOfType(_factory,typeof (TetrisItemFactory));
+        }
+    }
+}
